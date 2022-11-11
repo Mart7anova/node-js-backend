8d472fa73b3916e00c15c6b36dda96b0cf4a89af
Mart7anova <mart7anova@gmail.com>
2022-11-11 16:30:21 +0300

    refactor: userController using switch-case

--- usersController.js
+++ usersController.js
@@ -1,10 +1,13 @@
 const {addUsers, getUsers} = require("./repository");
 
 exports.usersController = (req, res) => {
-    if (req.method === 'POST') {
-        addUsers(3, 'Andey')
-        res.write(JSON.stringify({success: true}));
-    } else {
-        res.write(JSON.stringify(getUsers()));
+    switch (req.method) {
+        case 'POST':
+            addUsers(3, 'Andey')
+            res.write(JSON.stringify({success: true}));
+            break;
+        default :
+            res.write(JSON.stringify(getUsers()));
+            break;
     }
 }
